11b22b48aeec1760f2ce9313541ef1e847358051
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-10-06 19:44:08 +0700

    Test commit

--- src/main/java/com/second_hand_auction_system/SecondHandAuctionSystemBeApplication.java
+++ src/main/java/com/second_hand_auction_system/SecondHandAuctionSystemBeApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecondHandAuctionSystemBeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SecondHandAuctionSystemBeApplication.class, args);
-	}
+    //Test
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandAuctionSystemBeApplication.class, args);
+    }
 
 }
