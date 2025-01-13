45199c0c37ff9211a9ffc5993c99d36f69d86c9e
Anhquan <anhquanpro332002@gmail.com>
2025-01-13 14:56:36 +0700

    vps

--- src/main/java/com/second_hand_auction_system/configurations/CorsFilter.java
+++ src/main/java/com/second_hand_auction_system/configurations/CorsFilter.java
@@ -16,21 +16,10 @@ import java.util.List;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter extends OncePerRequestFilter {
 
-    private static final List<String> ALLOWED_ORIGINS = List.of(
-            "http://localhost:5173",
-            "https://auction-system-plum.vercel.app"
-    );
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String origin = request.getHeader("Origin");
-
-        // Kiểm tra Origin có trong danh sách cho phép
-        if (origin != null && ALLOWED_ORIGINS.contains(origin)) {
-            response.setHeader("Access-Control-Allow-Origin", origin);
-        }
-
-        // Thiết lập các header khác
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
